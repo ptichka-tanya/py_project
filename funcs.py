8c76eb290f0f45e72d6dfaf6f8b2e932dcc06e82
ptichka-tanya <t.n.sinitsina@gmail.com>
2023-12-04 19:44:31 +0300

    add function summary

---
+++ funcs.py
@@ -0,0 +1,8 @@
+def num_summary(first_num, second_num):
+    return first_num + second_num
+
+
+#user_input_1 = int(input("введите число-1: "))
+#user_input_2 = int(input("введите число-2: "))
+
+#print(num_summary(user_input_1, user_input_2))
