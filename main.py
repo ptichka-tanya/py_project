af0e88d13b373874a8c43638721237077708eca1
ptichka-tanya <t.n.sinitsina@gmail.com>
2023-12-04 21:15:05 +0300

    add main with function subtraction

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from funcs import num_summary
-
+from funcs import num_summary, num_subtraction
 
 user_input_1 = int(input("введите число-1: "))
 user_input_2 = int(input("введите число-2: "))
 
 print(num_summary(user_input_1, user_input_2))
+print(num_subtraction(user_input_1, user_input_2))
